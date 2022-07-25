3ef96ef3c28a247bc24856f1d3a7d4efacd10a9b
yong <tkddbs1644@gmail.com>
2022-07-25 15:07:16 +0900

    chore: 디렉토리 구조 설정

---
+++ src/pages/404.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+const Custom404: NextPage = () => {
+  return (
+    <div>
+      <h1>404 Not Found</h1>
+    </div>
+  );
+};
+
+export default Custom404;
--- src/service/sampleApi/index.ts
+++
@@ -1,5 +0,0 @@
-import axios from 'axios';
-
-export const getPosts = async () => {
-  return await axios.get('https://jsonplaceholder.typicode.com/posts');
-};
