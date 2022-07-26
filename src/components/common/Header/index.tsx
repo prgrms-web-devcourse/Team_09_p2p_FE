b043addbe120a82a53bda8d079b39c46bed8831c
jinist94 <325days@naver.com>
2022-07-27 05:50:53 +0900

    feat: Header 컴포넌트에 PageContainer 적용 및 관련 코드 수정

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -1,57 +1,50 @@
 import styled from '@emotion/styled';
 import Link from 'next/link';
 import React from 'react';
-import Button from '~/components/atom/Button';
+import { PageContainer, Button } from '~/components/atom';
 import Logo from '~/components/atom/Logo';
 import theme from '~/styles/theme';
 
 const Header = () => {
   return (
-    <>
-      <PageContainer>
-        <Inner>
-          <LeftArea>
-            <Link href="/" passHref>
-              <a>
-                <Logo width={130} height={35} />
-              </a>
-            </Link>
-            <Category>
-              <li>
-                <Link href="/course">여행코스</Link>
-              </li>
-              <li>
-                <Link href="/place">추천장소</Link>
-              </li>
-            </Category>
-          </LeftArea>
+    <PageContainer>
+      <Inner>
+        <LeftArea>
+          <Link href="/" passHref>
+            <a>
+              <Logo width={130} height={35} />
+            </a>
+          </Link>
+          <Category>
+            <li>
+              <Link href="/course">여행코스</Link>
+            </li>
+            <li>
+              <Link href="/place">추천장소</Link>
+            </li>
+          </Category>
+        </LeftArea>
 
-          <div>
-            <input placeholder="지역, 장소를 입력해주세요" />
-          </div>
+        <div>
+          <input placeholder="지역, 장소를 입력해주세요" />
+        </div>
 
-          <Buttons>
-            <Link href="/course/create" passHref>
-              <Button>코스등록</Button>
-            </Link>
-            <Link href="/login" passHref>
-              <Button buttonType="borderPrimary">로그인</Button>
-            </Link>
-          </Buttons>
-        </Inner>
-      </PageContainer>
-    </>
+        <Buttons>
+          <Link href="/course/create" passHref>
+            <Button>코스등록</Button>
+          </Link>
+          <Link href="/login" passHref>
+            <Button buttonType="borderPrimary">로그인</Button>
+          </Link>
+        </Buttons>
+      </Inner>
+    </PageContainer>
   );
 };
 
 export default Header;
 
 const { mainColor } = theme.color;
-
-const PageContainer = styled.div`
-  width: 1156px;
-  margin: 0 auto;
-`;
 
 const Inner = styled.div`
   display: flex;
