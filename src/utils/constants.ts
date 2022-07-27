fa1194d7add2c523f4489ffd5bee74b32caf1a02
jinist94 <jay@Hamui-MacBook-Pro.local>
2022-07-28 06:16:14 +0900

    feat: font size, color 상수화

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,23 @@
-export const FONT_SIZE = {
-  normal: '16'
+import theme from '~/styles/theme';
+
+const { fontGray, fontDarkGray, fontDarkBlack } = theme.color;
+
+export const FONT_SIZES = {
+  xs: 14,
+  sm: 16,
+  md: 18,
+  lg: 20,
+  xl: 24
+};
+
+export const TITLE_SIZES = {
+  sm: 24,
+  md: 32,
+  lg: 40
+};
+
+export const FONT_COLORS: { [key: string]: string } = {
+  gray: fontGray,
+  darkGray: fontDarkGray,
+  dark: fontDarkBlack
 };
