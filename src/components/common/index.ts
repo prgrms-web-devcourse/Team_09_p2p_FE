2b4fd99f801126716b3d959956c787cec0da9536
jinist94 <jay@Hamui-MacBook-Pro.local>
2022-07-28 18:16:52 +0900

    feat: 컴포넌트 default as 추가

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,2 +1,5 @@
 export { default as Header } from './Header';
+export { default as LikeCount } from './LikeCount';
 export { default as CourseItem } from './CourseItem';
+export { default as PlaceItem } from './PlaceItem';
+export { default as BookmarkIcon } from './BookmarkIcon';
