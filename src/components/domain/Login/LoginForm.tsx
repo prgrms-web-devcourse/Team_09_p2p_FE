ed0f299ff449c765e3e7791520420439173302e9
yong <tkddbs1644@gmail.com>
2022-07-28 14:36:23 +0900

    feat: LoginForm page 구현

--- src/components/domain/Login/LoginForm.tsx
+++ src/components/domain/Login/LoginForm.tsx
@@ -1,8 +1,12 @@
 import Link from 'next/link';
-import React from 'react';
+import React, { useMemo } from 'react';
 import { useFormik } from 'formik';
 import { SchemaOf, object, string } from 'yup';
+import styled from '@emotion/styled';
+import theme from '~/styles/theme';
+import { PageContainer, Logo, Input, Label, Button } from '~/components/atom';
 import { LoginValues } from '~/types';
+import { Form } from '~/components/common';
 
 const LoginValidationSchema: SchemaOf<LoginValues> = object().shape({
   email: string().email('이메일 형식이 아닙니다.').required('이메일을 입력해주세요.'),
@@ -18,62 +22,83 @@ interface LoginFormProps {
   onSubmit: (data: LoginValues) => void;
 }
 
-const LoginForm: React.FC<LoginFormProps> = ({ onSubmit: trigger }) => {
-  const { values, handleChange, handleSubmit, errors, touched } = useFormik({
+const LoginForm: React.FC<LoginFormProps> = ({ onSubmit: handleSubmitAction }) => {
+  const { values, handleChange, handleSubmit, errors } = useFormik({
     initialValues,
     onSubmit: (data: LoginValues) => {
-      trigger && trigger(data);
+      handleSubmitAction && handleSubmitAction(data);
     },
     validationSchema: LoginValidationSchema
   });
 
+  const disableButton = useMemo(() => {
+    return Boolean(!values.email || !values.password || errors.email || errors.password);
+  }, [values.email, values.password, errors.email, errors.password]);
+
   return (
-    <div>
-      <h1>로고</h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="email" style={{ display: 'none' }}>
-          이메일
-        </label>
-        {touched.email && errors.email && <div>{errors.email}</div>}
-        <input
-          id="email"
-          name="email"
-          type="email"
-          placeholder="이메일"
-          required
-          value={values.email}
-          onChange={handleChange}
-        />
-        <label htmlFor="password" style={{ display: 'none' }}>
-          비밀번호
-        </label>
-        {touched.password && errors.password && <div>{errors.password}</div>}
-        <input
-          id="password"
-          name="password"
-          type="password"
-          placeholder="비밀번호"
-          required
-          value={values.password}
-          onChange={handleChange}
-        />
-        <button
-          disabled={Boolean(!values.email || !values.password || errors.email || errors.password)}
-          type="submit"
-        >
-          로그인
-        </button>
-      </form>
-      <div>
-        <span>
-          아직 회원이 아니신가요?{' '}
-          <Link href="/signup" passHref>
-            회원가입
-          </Link>
-        </span>
-      </div>
-    </div>
+    <Layout>
+      <Container>
+        <Logo width={150} height={100} />
+        <FormWrapper>
+          <Form onSubmit={handleSubmit}>
+            <Label htmlFor="email" text="이메일" display="none" />
+            <Input
+              name="email"
+              type="email"
+              placeholder="이메일"
+              required
+              value={values.email}
+              onChange={handleChange}
+            />
+            <Label htmlFor="password" text="비밀번호" display="none" />
+            <Input
+              name="password"
+              type="password"
+              placeholder="비밀번호"
+              required
+              value={values.password}
+              onChange={handleChange}
+            />
+            <Button type="submit" disabled={disableButton}>
+              로그인
+            </Button>
+          </Form>
+        </FormWrapper>
+        <div>
+          {/* TODO - Text컴포넌트 머지 후 변경 */}
+          <span>
+            아직 회원이 아니신가요?{' '}
+            <Link href="/signup" passHref>
+              회원가입
+            </Link>
+          </span>
+        </div>
+      </Container>
+    </Layout>
   );
 };
 
 export default LoginForm;
+
+const Layout = styled(PageContainer)`
+  height: calc(100vh - 100px);
+  display: flex;
+  align-items: center;
+`;
+
+const Container = styled.div`
+  margin: 0 auto;
+  width: 568px;
+  height: 580px;
+  border: 1px solid ${theme.color.borderDarkGray};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const FormWrapper = styled.div`
+  box-sizing: border-box;
+  width: 100%;
+  padding: 60px;
+`;
