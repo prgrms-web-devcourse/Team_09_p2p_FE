9d4172dd16c21c2f9a66db747bb7d2f4792d9a2a
jinist94 <325days@naver.com>
2022-07-31 03:02:52 +0900

    feat: PlaceList 컴포넌트 분리

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -7,3 +7,4 @@ export { default as Form } from './Form';
 export { default as SortFilter } from './SortFilter';
 export { default as CategoryTitle } from './CategoryTitle';
 export { default as CourseList } from './CourseList';
+export { default as PlaceList } from './PlaceList';
