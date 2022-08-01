68dee4542b51fc737a64c583de517957388cbe83
jinist94 <325days@naver.com>
2022-08-02 06:17:01 +0900

    feat: Sidebar에 사용할 아이콘 추가

--- src/components/atom/Icon/types.ts
+++ src/components/atom/Icon/types.ts
@@ -4,7 +4,12 @@ export const ICON_URLS = {
   arrow: '/assets/icons/arrow.svg',
   calendar: '/assets/icons/calendar.svg',
   marker: '/assets/icons/marker.svg',
-  route: '/assets/icons/route.svg'
+  route: '/assets/icons/route.svg',
+  bookmarkActive: '/assets/icons/bookmark-active.svg',
+  bookmarkInactive: '/assets/icons/bookmark-inactive.svg',
+  heartActive: '/assets/icons/heart-active.svg',
+  heartInactive: '/assets/icons/heart-inactive.svg',
+  share: '/assets/icons/share.svg'
 };
 
 export type IconName = keyof typeof ICON_URLS;
