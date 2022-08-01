cdaed445b717f4652a50ceafa267cab1b4dda8cb
yong <tkddbs1644@gmail.com>
2022-08-01 18:12:46 +0900

    chore: index 위치 수정

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -4,4 +4,3 @@ export { default as CourseItem } from './CourseItem';
 export { default as PlaceItem } from './PlaceItem';
 export { default as BookmarkIcon } from './BookmarkIcon';
 export { default as Form } from './Form';
-export { default as Modal } from './Modal';
