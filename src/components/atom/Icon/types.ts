8a7a221f8cf0468edd2fe56e088aa37278ddaff4
yong <tkddbs1644@gmail.com>
2022-08-02 16:40:47 +0900

    feat: SearchBar 로직 구현

--- src/components/atom/Icon/types.ts
+++ src/components/atom/Icon/types.ts
@@ -4,7 +4,8 @@ export const ICON_URLS = {
   arrow: '/assets/icons/arrow.svg',
   calendar: '/assets/icons/calendar.svg',
   marker: '/assets/icons/marker.svg',
-  route: '/assets/icons/route.svg'
+  route: '/assets/icons/route.svg',
+  search: '/assets/icons/search.svg'
 };
 
 export type IconName = keyof typeof ICON_URLS;
--- src/components/common/SearchInput/index.tsx
+++ src/components/common/SearchInput/index.tsx
@@ -1,35 +1,41 @@
 import styled from '@emotion/styled';
-import React, { ChangeEvent, useState } from 'react';
+import React, { FormEvent, useState } from 'react';
 import { Icon, Input } from '~/components/atom';
 
 interface SearchInputProps {
   placeholder?: string;
+  onSearch: (keyword: string) => void;
 }
 
-const SearchInput = ({ placeholder = '검색어를 입력해주세요.' }: SearchInputProps) => {
+const SearchInput = ({ onSearch, placeholder = '검색어를 입력해주세요.' }: SearchInputProps) => {
   const [keyword, setKeyword] = useState('');
 
-  const handleChangeKeyword = (e: ChangeEvent<HTMLInputElement>) => {
-    setKeyword(e.target.value);
+  const handleSearch = async (e: FormEvent) => {
+    e.preventDefault();
+    if (!keyword) return;
+    await onSearch(keyword);
+    setKeyword('');
   };
 
   return (
-    <Container>
+    <FormContainer onSubmit={handleSearch}>
       <Icon name="search" />
-      <StyledSearchInput placeholder={placeholder} />
-    </Container>
+      <Input
+        name="search-input"
+        placeholder={placeholder}
+        value={keyword}
+        onChange={(e) => setKeyword(e.target.value)}
+        autoComplete="off"
+      />
+    </FormContainer>
   );
 };
 
 export default SearchInput;
 
-const Container = styled.div`
+const FormContainer = styled.form`
   width: 356px;
   height: 55px;
   padding: 13px;
   display: flex;
-`;
-
-const StyledSearchInput = styled.input`
-  width: 100%;
 `;
