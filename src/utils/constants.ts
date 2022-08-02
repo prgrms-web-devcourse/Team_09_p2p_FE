ae34474f0d429c7184cecd99bb2a81f8028f296a
hwoo3303 <hwoo3303@naver.com>
2022-08-02 10:47:58 +0900

    feat: CloseIcon, PlusIcon 컴포넌트 추가

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -25,5 +25,7 @@ export const FONT_COLORS: { [key: string]: string } = {
 export const ICON_URLS: { [key: string]: string } = {
   heart: '/assets/icons/heart.svg',
   bookmark: '/assets/icons/bookmark.svg',
-  arrow: '/assets/icons/arrow.svg'
+  arrow: '/assets/icons/arrow.svg',
+  close: '/assets/icons/close.svg',
+  plus: '/assets/icons/plus.svg'
 };
