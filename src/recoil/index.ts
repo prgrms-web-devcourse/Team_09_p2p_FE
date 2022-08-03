5e07a590ba57d65dfb6f7dd2592792cce991b87f
jinist94 <325days@naver.com>
2022-08-04 03:31:55 +0900

    feat: recoil state 세팅

--- src/recoil/index.ts
+++ src/recoil/index.ts
@@ -1,1 +1,23 @@
-//TODO
+import { atom } from 'recoil';
+
+interface User {
+  id: number | null;
+  nickname: string | null;
+  profileImage: string | null;
+}
+interface UserState {
+  accessToken: string | null;
+  user: User;
+}
+
+export const userState = atom<UserState>({
+  key: 'LoginState',
+  default: {
+    accessToken: null,
+    user: {
+      id: 1,
+      nickname: 'jinist',
+      profileImage: null
+    }
+  }
+});
