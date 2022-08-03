8c9e42f703bc8871413401bf51d75927539e5f72
yong <tkddbs1644@gmail.com>
2022-08-03 18:57:04 +0900

    feat: SelectTags 컴포넌트 구현

---
+++ src/components/common/SelectTags/Tags.tsx
@@ -0,0 +1,44 @@
+import styled from '@emotion/styled';
+import React from 'react';
+import { Text } from '~/components/atom';
+import theme from '~/styles/theme';
+
+interface TagsProps {
+  tagName: string;
+  tags: string[];
+}
+
+const Tags = ({ tagName, tags }: TagsProps) => {
+  return (
+    <List key={tagName}>
+      <Text size="md" fontWeight={600} style={{ marginRight: '30px' }}>
+        {tagName}
+      </Text>
+      {tags.map((tag) => (
+        <Tag key={tag}>
+          <Text size="md">{tag}</Text>
+        </Tag>
+      ))}
+    </List>
+  );
+};
+
+export default Tags;
+
+const List = styled.ul`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+`;
+
+const Tag = styled.li`
+  border: 1px solid ${theme.color.mainBackground};
+  border-radius: 20px;
+  padding: 6px 15px;
+  color: ${theme.color.fontDarkGray};
+  cursor: pointer;
+  :hover {
+    border: 1px solid ${theme.color.mainColor};
+    color: ${theme.color.mainColor};
+  }
+`;
---
+++ src/components/common/SelectTags/index.tsx
@@ -0,0 +1,31 @@
+import styled from '@emotion/styled';
+import React, { CSSProperties } from 'react';
+import theme from '~/styles/theme';
+import { TAGS } from '~/utils/constants';
+import Tags from './Tags';
+
+interface SelectTagsProps {
+  style: CSSProperties;
+}
+
+const SelectTags = ({ ...props }: SelectTagsProps) => {
+  return (
+    <Container {...props}>
+      {Object.entries(TAGS).map(([tagName, tags]) => (
+        <Tags key={tagName} tagName={tagName} tags={tags} />
+      ))}
+    </Container>
+  );
+};
+
+export default SelectTags;
+
+const Container = styled.div`
+  box-sizing: border-box;
+  background-color: ${theme.color.mainBackground};
+  border-radius: 8px;
+  padding: 25px 45px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
