0346379091de8b515c11cf5fcf8614112b4e0948
yong <tkddbs1644@gmail.com>
2022-08-04 19:19:49 +0900

    feat: Api 기본클래스 구현

---
+++ src/service/core/WebStorage.ts
@@ -0,0 +1,28 @@
+type TokenKey = 'jwt-token';
+
+export default class WebStorage {
+  private static readonly tokenKey: TokenKey = 'jwt-token';
+
+  static getToken(): string | null {
+    if (typeof window !== 'undefined') {
+      const value = window.localStorage.getItem(this.tokenKey);
+      if (value) {
+        return JSON.parse(value);
+      }
+      return null;
+    }
+    return null;
+  }
+
+  static setToken(token: string): void {
+    if (typeof window !== 'undefined') {
+      window.localStorage.setItem(this.tokenKey, token);
+    }
+  }
+
+  static removeItem(): void {
+    if (typeof window !== 'undefined') {
+      window.localStorage.removeItem(this.tokenKey);
+    }
+  }
+}
