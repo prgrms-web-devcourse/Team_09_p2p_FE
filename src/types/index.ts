08289a9c8a2be17ecd17b0e137949a1534603bde
yong <tkddbs1644@gmail.com>
2022-08-05 20:10:42 +0900

    fix: Regions types를 object[] 에서 Region[]으로 변경

--- src/types/index.ts
+++ src/types/index.ts
@@ -18,9 +18,24 @@ export type LoginValues = {
   readonly password: string;
 };
 
-export type Region = {
-  text: string;
-};
+export type Region =
+  | '서울'
+  | '인천'
+  | '대구'
+  | '대전'
+  | '광주'
+  | '부산'
+  | '울산'
+  | '경기'
+  | '세종'
+  | '강원'
+  | '충북'
+  | '충남'
+  | '경북'
+  | '경남'
+  | '전북'
+  | '전남'
+  | '제주';
 
 export type PlacePost = {
   id: number;
