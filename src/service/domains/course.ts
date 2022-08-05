59ae6559133d0c4be7dac7fe4f02bf6847f098be
hwoo3303 <hwoo3303@naver.com>
2022-08-06 07:19:11 +0900

    feat: course 도메인 API 프로토타입 구현

---
+++ src/service/domains/course.ts
@@ -0,0 +1,102 @@
+import Api from '~/service/core/Api';
+
+type CourseReadingType = {
+  readonly placeId?: number;
+  readonly keyword?: string;
+  readonly region?: string;
+  readonly spot?: string[];
+  readonly theme?: string[];
+  readonly page: number;
+  readonly size: number;
+  readonly sort: string;
+};
+
+type CourseSearchBookmarked = {
+  readonly page: number;
+  readonly size: number;
+  readonly sort?: string;
+  readonly userId: number;
+};
+class CourseApi extends Api {
+  private path = '/courses';
+
+  create = async (formData: FormData) => {
+    try {
+      const response = await this.authInstance.post(`${this.path}/`, formData);
+      console.log(response);
+      return response.data;
+    } catch (e) {
+      console.error(`코스 등록 오류: ${e}`);
+    }
+  };
+
+  read = async (courseId: string) => {
+    /* try {
+      const response = await this.baseInstance.get(`${this.path}/${courseId}`);
+      console.log(response);
+      return response.data;
+    } catch (e) {
+      console.error(`코스 상세 조회 오류: ${e}`);
+    } */
+  };
+
+  update = async (formData: FormData) => {
+    /* try {
+      const response = await this.authInstance.put(this.path, formData);
+      console.log(response);
+      return response.data;
+    } catch (e) {
+      console.error(`코스 수정 에러: ${e}`);
+    } */
+  };
+
+  delete = async (courseId: string) => {
+    /* try {
+      const response = await this.authInstance.delete(`${this.path}/${courseId}`);
+      console.log(response);
+      return response.data;
+    } catch (e) {
+      console.error(`코스 삭제 에러: ${e}`);
+    } */
+  };
+
+  search = async (paramData: CourseReadingType) => {
+    /* try {
+      const response = await this.baseInstance.get(this.path, {
+        params: {
+          placeId: paramData.placeId,
+          keyword: paramData.keyword,
+          region: paramData.region,
+          spot: paramData.spot,
+          theme: paramData.theme,
+          page: paramData.page,
+          size: paramData.size,
+          sort: paramData.sort
+        }
+      });
+      console.log(response);
+      return response.data;
+    } catch (e) {
+      console.error(`코스 목록 조회 오류: ${e}`);
+    } */
+  };
+
+  searchBookmarked = async (paramData: CourseSearchBookmarked) => {
+    /* try {
+      const response = await this.baseInstance.get(this.path, {
+        params: {
+          page: paramData.page,
+          size: paramData.size,
+          sort: paramData.sort,
+          userId: paramData.userId
+        }
+      });
+      console.log(response);
+      return response.data;
+    } catch (e) {
+      console.error(`코스 목록 조회 오류: ${e}`);
+    } */
+  };
+}
+
+export default new CourseApi();
