698b671f7bdc57149aff23007616a5b4e6ccad2a
jinist94 <325days@naver.com>
2022-08-06 17:35:32 +0900

    refactor: User type 파일 분리

--- src/recoil/index.ts
+++ src/recoil/index.ts
@@ -1,14 +1,5 @@
 import { atom } from 'recoil';
-interface User {
-  id: number | null;
-  nickname: string | null;
-  profileImage: string | null;
-}
-interface UserState {
-  accessToken: string | null;
-  user: User;
-}
-
+import { UserState } from '~/types/user';
 // 여기서 localstorage 불러와서 사용 안됨
 
 export const userState = atom<UserState>({
