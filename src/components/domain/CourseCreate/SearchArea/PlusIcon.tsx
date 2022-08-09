e948c73bc09b271c9d9fe8d47353028f03e80336
hwoo3303 <hwoo3303@naver.com>
2022-08-10 02:29:55 +0900

    feat: 아이콘 onClick 이벤트 추가

--- src/components/domain/CourseCreate/SearchArea/PlusIcon.tsx
+++ src/components/domain/CourseCreate/SearchArea/PlusIcon.tsx
@@ -1,17 +1,17 @@
 import styled from '@emotion/styled';
-import { Icon } from '~/components/atom';
+import IconButton from '~/components/atom/Icon/IconButton';
 
 interface PlusIconProps {
   onClick?: () => void;
 }
 
 const PlusIcon = ({ onClick }: PlusIconProps) => {
-  return <StyledIcon name="plus" size={12} />;
+  return <StyledIcon onClick={onClick as () => void} name="plus" size={12} />;
 };
 
 export default PlusIcon;
 
-const StyledIcon = styled(Icon)`
+const StyledIcon = styled(IconButton)`
   position: absolute;
   top: 25px;
   right: 31px;
