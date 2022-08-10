fda5a52e95ed689f18131134093195700a8cd10a
yong <tkddbs1644@gmail.com>
2022-08-10 16:31:53 +0900

    feat: 유저 정보 페이지 정보 수정 기능 구현

--- src/pages/userinfo/[id]/edit.tsx
+++ src/pages/userinfo/[id]/edit.tsx
@@ -8,7 +8,6 @@ import UserEditForm, { UserEditFormValues } from '~/components/domain/UserInfo/E
 import { useUser } from '~/hooks/useUser';
 import { UserApi } from '~/service';
 import theme from '~/styles/theme';
-import { isNumber } from '~/utils/converter';
 
 const UserinfoEdit: NextPage = () => {
   const { currentUser } = useUser();
@@ -23,29 +22,23 @@ const UserinfoEdit: NextPage = () => {
     const { nickname: prevNickname, birth: prevBirth, sex: prevSex } = initialValues;
     const { nickname, birth, sex } = data;
     if (prevNickname === nickname && prevBirth === birth && prevSex === sex) {
-      // 바로 이동시키거나
       router.push(`/userinfo/${userId}`);
-      // 알림 모달을 띄우기
-      // ConfirmModal...
       return;
     }
 
     try {
       const response = await UserApi.edit({ nickname, birth, sex });
       if (response.status === 200) {
-        // 바로 이동시키거나
         router.push(`/userinfo/${userId}`);
-        // 알림 모달을 띄우기
-        // ConfirmModal...
       }
     } catch (e) {
-      console.error('정보 수정에 실패했어요.', e);
       // TODO 이후 status code에 따른 예외처리
+      console.error(e);
     }
   };
 
   const getUserData = async () => {
-    const userInfo = await UserApi.getUser();
+    const userInfo = await UserApi.getUser(userId);
     const { email, nickname, sex, birth } = userInfo;
     setInitialValues(() => ({
       email,
