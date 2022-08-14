6771a0a1e7b3ab6b7de4ec84c5b25f1852e9deb1
jinist94 <325days@naver.com>
2022-08-15 04:16:10 +0900

    feat: _app.tsx meta tag 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import type { NextPage } from 'next';
 import Layout from '~/components/common/Layout';
 import { ReactElement, ReactNode } from 'react';
 import { RecoilRoot } from 'recoil';
+import Head from 'next/head';
 
 export type NextPageWithLayout = NextPage & { getLayout?: (page: ReactElement) => ReactNode };
 type AppPropsWidthLayout = AppProps & { Component: NextPageWithLayout };
@@ -18,7 +19,23 @@ declare global {
 function MyApp({ Component, pageProps }: AppPropsWidthLayout) {
   const getLayout = Component.getLayout || ((page) => <Layout>{page}</Layout>);
 
-  return <RecoilRoot>{getLayout(<Component {...pageProps} />)}</RecoilRoot>;
+  return (
+    <>
+      <Head>
+        <title>우리의 여행코스 | 이곳저곳</title>
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="이곳저곳" />
+        <meta property="og:site_name" content="이곳저곳" />
+        <meta property="og:url" content="https://team-09-p2p-fe.vercel.app/" />
+        <meta property="og:image" content="/assets/images/og-image.png" />
+        <meta
+          property="og:description"
+          content="다양한 여행 코스 정보를 확인할 수 있는 이곳저곳!"
+        />
+      </Head>
+      <RecoilRoot>{getLayout(<Component {...pageProps} />)}</RecoilRoot>
+    </>
+  );
 }
 
 export default MyApp;
