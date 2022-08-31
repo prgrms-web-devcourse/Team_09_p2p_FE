dacba85224596e27888cdb0a1b8045739d7253f1
jinist94 <325days@naver.com>
2022-08-31 12:30:54 +0900

    refactor: Slider 컴포넌트 관련 중복코드 삭제 및 SliderContainer type 분리

--- src/components/common/SliderContainer/index.tsx
+++ src/components/common/SliderContainer/index.tsx
@@ -1,14 +1,17 @@
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import styled from '@emotion/styled';
 import { ReactNode } from 'react';
 import Slider from 'react-slick';
-import theme from '~/styles/theme';
+import { buttonStyle, buttonType } from './types';
 
 interface SliderContainerProps {
   children: ReactNode;
   dots?: boolean;
+  button?: buttonType;
 }
 
-const SliderContainer = ({ children, dots }: SliderContainerProps) => {
+const SliderContainer = ({ children, dots, button = 'normal' }: SliderContainerProps) => {
   const settings = {
     infinite: false,
     speed: 500,
@@ -16,14 +19,19 @@ const SliderContainer = ({ children, dots }: SliderContainerProps) => {
     slidesToScroll: 3,
     dots: dots
   };
-  return <StyledSlider {...settings}>{children}</StyledSlider>;
+  return (
+    <StyledSlider {...settings} button={button}>
+      {children}
+    </StyledSlider>
+  );
 };
 
 export default SliderContainer;
 
-const StyledSlider = styled(Slider)`
+const StyledSlider = styled(Slider)<SliderContainerProps>`
   margin-top: 24px;
-  height: 360px;
+
+  ${({ button }) => button && buttonStyle[button]}
 
   .slick-list {
     margin-left: -10px;
@@ -39,31 +47,17 @@ const StyledSlider = styled(Slider)`
     width: 50px;
     height: 50px;
     z-index: 100;
-    background-image: url('/assets/icons/thinArrow.svg');
-    background-size: 45px;
     background-repeat: no-repeat;
     background-position: center;
 
     &.slick-disabled {
       display: none !important;
     }
-    &:hover {
-      background-image: url('/assets/icons/thinArrow-active.svg');
-    }
-  }
-
-  .slick-prev {
-    left: -56px;
-    transform: translate(0, -50%) rotate(180deg);
   }
 
   .slick-next:before,
   .slick-prev:before {
     content: '';
-  }
-
-  .slick-next {
-    right: -56px;
   }
 
   .slick-dots {
