20d026dd5b3c956eb7c70bfded4e961ef13957b2
jinist94 <325days@naver.com>
2022-09-08 00:06:53 +0900

    feat: constants 파일 폴더로 분리

---
+++ src/utils/constants/images.ts
@@ -0,0 +1,27 @@
+export const IMAGE_URL = 'https://devcourse-f-s3-storage.s3.ap-northeast-2.amazonaws.com';
+
+export const USER_DEFAULT_IMAGE = `${IMAGE_URL}/assets/profile-default.jpg`;
+export const THUMBNAIL_DEFAULT_IMAGE = `${IMAGE_URL}/assets/images/image-not-found.png`;
+
+export const MARKER_IMAGE_URLS: { [key: string]: string } = {
+  academy: `${IMAGE_URL}/assets/place/academy.png`,
+  agency: `${IMAGE_URL}/assets/place/agency.png`,
+  bank: `${IMAGE_URL}/assets/place/bank.png`,
+  convenience: `${IMAGE_URL}/assets/place/convenience.png`,
+  culturalFacility: `${IMAGE_URL}/assets/place/culturalFacility.png`,
+  defaultPlace: `${IMAGE_URL}/assets/place/defaultPlace.png`,
+  gasStation: `${IMAGE_URL}/assets/place/gasStation.png`,
+  kindergarden: `${IMAGE_URL}/assets/place/kindergarden.png`,
+  parking: `${IMAGE_URL}/assets/place/parking.png`,
+  publicInstitutions: `${IMAGE_URL}/assets/place/publicInstitutions.png`,
+  school: `${IMAGE_URL}/assets/place/school.png`,
+  shopping: `${IMAGE_URL}/assets/place/shopping.png`,
+  subway: `${IMAGE_URL}/assets/place/subway.png`,
+  attractions: `${IMAGE_URL}/assets/place/attractions.png`,
+  accommodation: `${IMAGE_URL}/assets/place/accommodation.png`,
+  restaurant: `${IMAGE_URL}/assets/place/restaurant.png`,
+  cafe: `${IMAGE_URL}/assets/place/cafe.png`,
+  hospital: `${IMAGE_URL}/assets/place/hospital.png`,
+  pharmacy: `${IMAGE_URL}/assets/place/pharmacy.png`,
+  placeNumberSprite: `${IMAGE_URL}/assets/place/_place-number-sprite.png`
+};
